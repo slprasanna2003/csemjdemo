2224b27d40d0e4f8ad199fe7ae3b3821191b4d59
Admin <Admin@DESKTOP-QG2DJLM>
2022-11-18 15:45:50 +0530

    first commit to jenkins

---
+++ src/main/java/Mavenjenkins1/csemjdemo/App.java
@@ -0,0 +1,13 @@
+package Mavenjenkins1.csemjdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to jenkins world" );
+    }
+}
